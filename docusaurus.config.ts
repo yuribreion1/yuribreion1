c2700c56d8153c702ec23b6a52a715770160aa8e
Yuri Breion <yuribreion@gmail.com>
2025-11-22 16:20:47 +0100

    Setting the application url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://yuribreion1.github.io/yuribreion1',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
